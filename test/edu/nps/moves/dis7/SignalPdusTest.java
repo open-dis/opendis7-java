18e4b3eb98937d1e3e7f795670a0c040463e0e14
terry-norbraten <tnorb@comcast.net>
2020-06-07 14:52:50 -0700

    strengthen to test for anomaly seen during the AllPduRoundTripTest with the special case Signal PDUs

--- test/edu/nps/moves/dis7/SignalPdusTest.java
+++ test/edu/nps/moves/dis7/SignalPdusTest.java
@@ -4,106 +4,160 @@
  */
 package edu.nps.moves.dis7;
 
-import edu.nps.moves.dis7.utilities.DisNetworking;
+import edu.nps.moves.dis7.utilities.DisThreadedNetIF;
 import edu.nps.moves.dis7.utilities.PduFactory;
+import edu.nps.moves.dis7.utilities.stream.PduPlayer;
+import edu.nps.moves.dis7.utilities.stream.PduRecorder;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * This is an important unit test as it examines the validity of our two special
+ * case signal PDUs
+ *
+ * @author tdnorbra@nps.edu
+ */
 @DisplayName("Signal Pdus Test")
-public class SignalPdusTest
-{
-  Pdu receivedPdu;
-  DisNetworking disnet;
-  
-  @BeforeAll
-  public static void setUpClass()
-  {
-    System.out.println("SignalPdusTest");
-  }
+public class SignalPdusTest {
 
-  @AfterAll
-  public static void tearDownClass()
-  {
-  }
+    static DisThreadedNetIF netif;
+    static List<Pdu> receivedPdus;
+    static PduRecorder recorder;
+    PduFactory pduFac;
+    List<Pdu> sentPdus;
+    byte[] buff;
+    int size;
 
-  @BeforeEach
-  public void setUp()
-  {
-      disnet = new DisNetworking();
-      setUpReceiver();
-  }
-
-  @AfterEach
-  public void tearDown()
-  {
-//      disnet.stop();
-//      disnet = null;
-  }
-
-  @Test
-  public void testRoundTrip()
-  {
-    PduFactory factory = new PduFactory();
-    SignalPdu sigPdu = factory.makeSignalPdu();  // empty contents
-
-    SignalPdu sigPdu2 = factory.makeSignalPdu();
-    sigPdu2.setEncodingScheme((short) 0x1111);
-    sigPdu2.setSampleRate(0x22222222);
-    sigPdu2.setSamples((short) 0x3333);
-    sigPdu2.setData("SignalPdu-testdata".getBytes());
-
-    IntercomSignalPdu isig = factory.makeIntercomSignalPdu();
-    IntercomSignalPdu isig2 = factory.makeIntercomSignalPdu();
-    isig2.setEncodingScheme((short) 0x1111);
-    isig2.setSampleRate(0x22222222);
-    isig2.setSamples((short) 0x3333);
-    isig2.setData("IntercomSignalPdu-testdata".getBytes());
-    
-    try {
-      Thread.sleep(250l); // make sure receiver is listening
-      disnet.sendPdu(sigPdu);
-      disnet.sendPdu(sigPdu2);
-      disnet.sendPdu(isig);
-      disnet.sendPdu(isig2);
-      Thread.sleep(100l);
-    }
-    catch (Exception ex) {
-      System.err.println("Error sending Multicast: " + ex.getLocalizedMessage());
-      System.exit(1);
+    @BeforeAll
+    public static void setUpClass() throws IOException {
+        System.out.println("SignalPdusTest");
+        recorder = new PduRecorder(); // default dir
+        netif = recorder.getDisThreadedNetIF();
+        netif.addListener(pdu -> handleReceivedPdu(pdu));
     }
 
-    // Compare
-    // If we made it this far, we've sent and received.  Now compare.
-    assertTrue(receivedPdu != null, "No response from network");
-    assertTrue(receivedPdu.equals(sigPdu),"Sent and received pdus not identical");
+    @AfterAll
+    public static void tearDownClass() throws IOException {
+        recorder.end(); // kills the netif as well
+        recorder = null;
+        netif = null;
+
+        receivedPdus.clear();
+        receivedPdus = null;
+    }
+
+    @BeforeEach
+    public void setUp() {
+        sentPdus = new ArrayList<>();
+        receivedPdus = new ArrayList<>();
+        pduFac = new PduFactory();
+
+        Pdu pdu = pduFac.makeSignalPdu();  // empty contents
+        sentPdus.add(pdu);
+
+        pdu = pduFac.makeSignalPdu();
+        ((SignalPdu) pdu).setEncodingScheme((short) 0x1111);
+        ((SignalPdu) pdu).setSampleRate(0x22222222);
+        ((SignalPdu) pdu).setSamples((short) 0x3333);
+        ((SignalPdu) pdu).setData("SignalPdu-testdata".getBytes());
+        sentPdus.add(pdu);
+
+        pdu = pduFac.makeIntercomSignalPdu();
+        sentPdus.add(pdu);
+
+        pdu = pduFac.makeIntercomSignalPdu();
+        ((IntercomSignalPdu) pdu).setEncodingScheme((short) 0x1111);
+        ((IntercomSignalPdu) pdu).setSampleRate(0x22222222);
+        ((IntercomSignalPdu) pdu).setSamples((short) 0x3333);
+        ((IntercomSignalPdu) pdu).setData("IntercomSignalPdu-testdata".getBytes());
+        sentPdus.add(pdu);
+
+        try {
+            Thread.sleep(250l); // give receive time to spool up
+            sentPdus.forEach(p -> netif.send(p));
+            Thread.sleep(250l); // give receiver time to process
+        } catch (InterruptedException ex) {
+            fail("NetIF Send: " + ex);
+        }
+    }
+
+    @AfterEach
+    public void tearDown() {
+        pduFac = null;
+        sentPdus.clear();
+        sentPdus = null;
+        buff = null;
+    }
+
+    @Test
+    public void testRoundTripNet() {
+
+        // Compare
+        assertEquals(sentPdus, receivedPdus, "Sent and received pdus not identical");
+
+        // Let's see how these unmarshall
+        receivedPdus.forEach(pdu -> {
+            try {
+                buff = pdu.marshal();
+                size = pdu.unmarshal(ByteBuffer.wrap(buff));
+                assertTrue(size > 0, "Unmarshalling error: Unmarshalled size: " + size);
+
+                // This also unmarshalls the pdu
+                pdu = pduFac.createPdu(buff);
+                assertNotNull(pdu, "Unmarshalling error " + pdu);
+            } catch (Exception ex) {
+                Logger.getLogger(SignalPdusTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+        receivedPdus.clear();
+
+        System.out.println("testRoundTripNet finished");
+    }
+
+    @Test
+    public void testRoundTripLog() throws IOException {
+
+        PduPlayer player = new PduPlayer(netif.getMcastGroup(), netif.getDisPort(), Path.of(recorder.getLogFile()));
+        player.sendToNet(false);
+        player.addRawListener(ba -> {
+            if (ba != null) {
+                Pdu pdu = pduFac.createPdu(ba);
+                receivedPdus.add(pdu);
+            } else {
+                player.end();
+            }
+        });
+        player.startResume();
     
-    System.out.println("SignalPdusTest finished");
-  }
+        // Compare
+        assertEquals(sentPdus, receivedPdus, "Sent and received pdus not identical");
 
-  private void setUpReceiver()
-  {
-    Thread rcvThread = new Thread(() -> {
-      try {
-        receivedPdu = disnet.receivePdu();  // blocks
-      }
-      catch (IOException ex) {
-        System.err.println("Error receiving Multicast: " + ex.getLocalizedMessage());
-        System.exit(1);
-      }
-    });
+        System.out.println("testRoundTripLog finished");
+    }
 
-    rcvThread.setPriority(Thread.NORM_PRIORITY);
-    rcvThread.setDaemon(true);
-    rcvThread.start();
-  }
+    static private void handleReceivedPdu(Pdu pdu) {
+        receivedPdus.add(pdu);
+    }
 
-  public static void main(String[] args)
-  {
-    SignalPdusTest spt = new SignalPdusTest();
-    spt.setUp();
-    spt.testRoundTrip();
-//    cpt.tearDown();.testRoundTrip();
-  }
+    public static void main(String[] args) throws IOException {
+        SignalPdusTest.setUpClass();
+        SignalPdusTest spt = new SignalPdusTest();
+
+        spt.setUp();
+        spt.testRoundTripNet();
+        spt.tearDown();
+
+        spt.setUp();
+        spt.testRoundTripLog();
+        spt.tearDown();
+        SignalPdusTest.tearDownClass();
+    }
 
 }
