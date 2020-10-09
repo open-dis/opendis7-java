c0efcb0a4becf61baf1699f0dac1e581eba5d179
Don Brutzman <brutzman@nps.edu>
2020-10-08 17:53:01 -0700

    accept Terry's changes

---
+++ src-generated/edu/nps/moves/dis7/entities/EntityTypeFactory.java
@@ -0,0 +1,43 @@
+package edu.nps.moves.dis7.entities;
+
+import edu.nps.moves.dis7.pdus.EntityType;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class EntityTypeFactory
+{
+  private static Map<Integer,String> uid2ClassNameMap = new HashMap<>();
+  static {
+    try {
+      InputStream ins = EntityTypeFactory.class.getResourceAsStream("uid2EntityClass.properties");
+      Properties prop = new Properties();
+      prop.load(ins);
+      prop.entrySet().stream().forEach(e->uid2ClassNameMap.put(Integer.parseInt(e.getKey().toString()), e.getValue().toString()));
+    }
+    catch( IOException ex) {
+      throw new RuntimeException (ex);
+    }
+  }
+  
+  public static EntityType makeEntity(int uid)
+  {
+    String name = uid2ClassNameMap.get(uid);
+    if(name==null)
+      return null;
+    
+    try {
+      Class cls = Class.forName(name);
+      Constructor<EntityType> constr = (Constructor<EntityType>) cls.getConstructors()[0];
+      return constr.newInstance();
+    }
+    catch(ClassNotFoundException | InstantiationException | IllegalAccessException | InvocationTargetException ex) {
+      System.err.println(name+"(uid "+uid+") could not be instantiated.  Check for proper entity jar(s) on classpath.");
+      return null;
+    }
+  }
+}
