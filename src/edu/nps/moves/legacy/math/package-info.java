471e27ab51dc88a892e1adba602fde7ee6ce0bfe
brutzman <brutzman@nps.edu>
2021-08-15 16:08:27 -0700

    package information

---
+++ src/edu/nps/moves/legacy/math/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Contains several useful math-related classes.
+ * Additional information appears in the <a href="README.TXT" target="_blank">README</a> file.
+ * 
+ * @see java.lang.Package
+ * @see <a href="https://stackoverflow.com/questions/22095487/why-is-package-info-java-useful">https://stackoverflow.com/questions/22095487/why-is-package-info-java-useful</a>
+ * @see <a href="https://stackoverflow.com/questions/624422/how-do-i-document-packages-in-java">https://stackoverflow.com/questions/624422/how-do-i-document-packages-in-java</a>
+ */
+
+package edu.nps.moves.legacy.math;
