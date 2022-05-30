0d3c00639caf3061d24c57d98bdde76139c7bfe0
brutzman <brutzman@nps.edu>
2022-05-30 11:56:47 -0700

    license block

--- src/edu/nps/moves/dis7/utilities/DisThreadedNetworkInterface.java
+++ src/edu/nps/moves/dis7/utilities/DisThreadedNetworkInterface.java
@@ -1,6 +1,7 @@
 /**
  * Copyright (c) 2008-2022, MOVES Institute, Naval Postgraduate School (NPS). All rights reserved.
- * This work is provided under a BSD open-source license, see project license.html and license.txt
+ * This work is provided under a BSD-style open-source license, see project
+ * <a href="https://savage.nps.edu/opendis7-java/license.html" target="_blank">license.html</a> and <a href="https://savage.nps.edu/opendis7-java/license.txt" target="_blank">license.txt</a>
  */
 package edu.nps.moves.dis7.utilities;
 
