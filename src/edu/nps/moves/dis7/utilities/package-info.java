f24d79fbb3e9bc59511ffd7cfcdffebebbcdab6b
brutzman <brutzman@nps.edu>
2025-01-22 23:16:08 -0800

    package info indicating manual authoring

--- src/edu/nps/moves/dis7/utilities/package-info.java
+++ src/edu/nps/moves/dis7/utilities/package-info.java
@@ -1,5 +1,12 @@
 /**
  * Utility classes supporting edu.nps.moves.dis7 library.
+ * This source code is manually authored in order to support autogeneration of new code libraries.
+ * <ul>
+ * <li>currently supports Java </li>
+ *  <li>expected future additions: Python, JavaScript, others </li>
+ * </ul>
+ * TODO: normalization of fields is onerous and repeatedly implemented across these classes,
+ * with continued refactoring as needed.
  * 
  * @see java.lang.Package
  * @see <a href="https://stackoverflow.com/questions/22095487/why-is-package-info-java-useful">https://stackoverflow.com/questions/22095487/why-is-package-info-java-useful</a>
